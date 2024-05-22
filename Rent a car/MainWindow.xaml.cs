7513ddc2bc5f028e5fed23d1a89e3e4d5482e3a3
Ali3384 <38331709+Ali3384@users.noreply.github.com>
2024-05-22 15:38:56 +0200

    Добавьте файлы проекта.

---
+++ Rent a car/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using ControlzEx.Theming;
+using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rent_a_car
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void ThemeChange(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if(toggleSwitch != null)
+            {
+                if(toggleSwitch.IsOn)
+                {
+                    ThemeManager.Current.ChangeTheme(this, "Dark.Steel");
+                }
+                else
+                {
+                    ThemeManager.Current.ChangeTheme(this, "Light.Steel");
+                }
+            }
+            ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.SyncWithAppMode;
+            ThemeManager.Current.SyncTheme();
+        }
+    }
+}
